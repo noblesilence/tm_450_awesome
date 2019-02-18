84402070fbc786f0751b96e7256df1fb86f6408a
Aye Aye Mon <aye2m@Ayes-Mac-mini.local>
2019-02-18 11:07:19 +0700

    Modify scroll easing function, add smooth scrolling to getting started button

--- js/custom.js
+++ js/custom.js
@@ -1,54 +1,78 @@
 /* HTML document is loaded. DOM is ready.
 -------------------------------------------*/
-$(function(){
+$(function() {
+  /* start typed element */
+  //http://stackoverflow.com/questions/24874797/select-div-title-text-and-make-array-with-jquery
+  var subElementArray = $.map($(".sub-element"), function(el) {
+    return $(el).text();
+  });
+  $(".element").typed({
+    strings: subElementArray,
+    typeSpeed: 30,
+    contentType: "html",
+    showCursor: false,
+    loop: true,
+    loopCount: true
+  });
+  /* end typed element */
 
-    /* start typed element */
-    //http://stackoverflow.com/questions/24874797/select-div-title-text-and-make-array-with-jquery
-    var subElementArray = $.map($('.sub-element'), function(el) { return $(el).text(); });    
-    $(".element").typed({
-        strings: subElementArray,
-        typeSpeed: 30,
-        contentType: 'html',
-        showCursor: false,
-        loop: true,
-        loopCount: true,
-    });
-    /* end typed element */
+  /* Smooth scroll and Scroll spy (https://github.com/ChrisWojcik/single-page-nav)    
+    ---------------------------------------------------------------------------------*/
 
-    /* Smooth scroll and Scroll spy (https://github.com/ChrisWojcik/single-page-nav)    
-    ---------------------------------------------------------------------------------*/ 
-    $('.templatemo-nav').singlePageNav({
-        offset: $(".templatemo-nav").height(),
-        filter: ':not(.external)',
-        updateHash: false
-    });
+  var navOffset = $(".templatemo-nav").height();
 
-    /* start navigation top js */
-    $(window).scroll(function(){
-        if($(this).scrollTop()>58){
-            $(".templatemo-nav").addClass("sticky");
+  $(".templatemo-nav").singlePageNav({
+    offset: navOffset,
+    easing: "easeInOutExpo",
+    speed: 1000,
+    filter: ":not(.external)",
+    updateHash: true
+  });
+
+  $("#aboutLink").click(function(e) {
+    e.preventDefault();
+    var target = this.hash,
+      $target = $(target);
+
+    $("html, body")
+      .stop()
+      .animate(
+        {
+          scrollTop: $target.offset().top - navOffset
+        },
+        1000,
+        "easeInOutExpo",
+        function() {
+          window.location.hash = target;
         }
-        else{
-            $(".templatemo-nav").removeClass("sticky");
-        }
-    });
-    
-    /* Hide mobile menu after clicking on a link
+      );
+  });
+
+  /* start navigation top js */
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 58) {
+      $(".templatemo-nav").addClass("sticky");
+    } else {
+      $(".templatemo-nav").removeClass("sticky");
+    }
+  });
+
+  /* Hide mobile menu after clicking on a link
     -----------------------------------------------*/
-    $('.navbar-collapse a').click(function(){
-        $(".navbar-collapse").collapse('hide');
-    });
-    /* end navigation top js */
+  $(".navbar-collapse a").click(function() {
+    $(".navbar-collapse").collapse("hide");
+  });
+  /* end navigation top js */
 
-    $('body').bind('touchstart', function() {});
+  $("body").bind("touchstart", function() {});
 
-    /* wow
+  /* wow
     -----------------*/
-    new WOW().init();
+  new WOW().init();
 });
 
 /* start preloader */
-$(window).load(function(){
-	$('.preloader').fadeOut(1000); // set duration in brackets    
+$(window).load(function() {
+  $(".preloader").fadeOut(1000); // set duration in brackets
 });
 /* end preloader */
